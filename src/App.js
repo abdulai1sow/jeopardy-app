38f4b30b22759bfc70fe3125cd50f2dd20d763fb
sowabdulai <abdulai-as@hotmail.com>
2022-10-31 20:31:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [question, setQuestion] = useState(null)
+
+  const getRandom = async () => {
+    try {
+      const res = await fetch(' http://jservice.io/api/random')
+      const data = await res.json()
+      setQuestion(data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getRandom}>
+        Random Quest
+      </button>
+
+      <div>
+        {}
+      </div>
     </div>
   );
 }
